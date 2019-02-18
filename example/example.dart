b5f7dcabe025b5e54930c5b9a1554699380e8407
Alexander Ryzhov <aryzhov07@gmail.com>
2019-02-18 12:09:32 -0800

    Moved example/lib/example.dart to example/

--- example/example.dart
+++ example/example.dart
@@ -1,1 +1,94 @@
-lib/example.dart
+import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
+
+void main() {
+  runApp(SampleApp());
+}
+
+class SampleApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return SampleAppState();
+  }
+
+}
+
+class SampleAppState extends State<SampleApp> {
+
+  DateTime alert;
+
+  @override
+  void initState() {
+    super.initState();
+    alert = DateTime.now().add(Duration(seconds: 10));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sample App'),
+        ),
+        body:
+
+        TimerBuilder.scheduled([alert],
+          builder: (context) {
+            // This function will be called once the alert time is reached
+            var now = DateTime.now();
+            var reached = now.compareTo(alert) >= 0;
+            final textStyle = Theme.of(context).textTheme.title;
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    reached ? Icons.alarm_on: Icons.alarm,
+                    color: reached ? Colors.red: Colors.green,
+                    size: 48,
+                  ),
+                  !reached ?
+                    TimerBuilder.periodic(
+                        Duration(seconds: 1),
+                        align: false,
+                        builder: (context) {
+                          // This function will be called every second until the alert time
+                          var now = DateTime.now();
+                          var remaining = alert.difference(now);
+                          return Text(formatDuration(remaining), style: textStyle,);
+                        }
+                    )
+                    :
+                    Text("Alert", style: textStyle),
+                  RaisedButton(
+                    child: Text("Reset"),
+                    onPressed: () {
+                      setState(() {
+                        alert = DateTime.now().add(Duration(seconds: 10));
+                      });
+                    },
+                  ),
+                ],
+              ),
+            );
+          }
+        ),
+      ),
+      theme: ThemeData(
+        backgroundColor: Colors.white
+      ),
+    );
+  }
+}
+
+String formatDuration(Duration d) {
+  String f(int n) {
+    return n.toString().padLeft(2, '0');
+  }
+
+  return "${f(d.inMinutes)}:${f(d.inSeconds%60)}";
+}
+
